e1db19ce68d417858e8a6aa211e55bd754ee1fc4
ganeswar-velvadapu <ganeshvelvadapu@gmail.com>
2025-07-18 01:29:04 +0530

    reading files done

--- operations/openAndReadFile.go
+++ operations/openAndReadFile.go
@@ -9,8 +9,8 @@ import (
 func OpenAndReadFile() {
 	// just for reference we can read files like this too but we need to specify buffer before hand (which in this case is 100 bytes) which is not ideal for real world
 	// file, err := os.Open("hello.txt")
-	cwd, _ := os.Getwd()
-	fmt.Println("Current working directory:", cwd)
+	// cwd, _ := os.Getwd()
+	// fmt.Println("Current working directory:", cwd)
 	// if err != nil {
 	// 	fmt.Println("Error opening file")
 	// }
@@ -27,7 +27,16 @@ func OpenAndReadFile() {
 
 	// fmt.Println(string(fileContentByte[:fileContent]))
 
-	data, err := os.ReadFile("hello.txt")
+	var fileName string
+	fmt.Print("Enter the file name: ")
+	_, err := fmt.Scanln(&fileName)
+
+	if err != nil {
+		fmt.Println("Error in reading file :", err)
+		return
+	}
+
+	data, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Println("Error in reading file :", err)
 		return
